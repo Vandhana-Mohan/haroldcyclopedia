0cac1358ecfaf235e1dce4587cef3dda0fe6ec2d
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-02-21 01:30:55 -0500

    tailwind change

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1> Haroldcyclopedia</h1>
+      <h1 class="text-lg font-medium"> Haroldcyclopedia</h1>
       <p class="text-3xl font-bold underline">here</p>
     </div>
   );
