df9cf23163c8abc5d3cf334cd70a2d84434341b1
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-02-28 16:53:30 -0500

    Added search bar

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
+import Search from "./components/common/Search"
+import "./App.css";
 
-import './App.css';
-
-const API_BASE = ""
+const API_BASE = "https://dummyjson.com/";
 
 function App() {
   const [base, setBase] = useState([]);
   useEffect(() => {
-  function getAPI(){
-    const url = `${API_BASE}`
-    fetch(url)
-    .then((results) => results.json())
-    .then((data) => {
-      console.log(data)
-      setBase(data.products)
-    })
-    .catch((error) => console.log(error));
-  }
-  getAPI()
-}, [])
-  
+    function getAPI() {
+      const url = `${API_BASE}/products/`;
+      fetch(url)
+        .then((result) => result.json())
+        .then((data) => {
+          console.log(data.products)
+          setBase(data.products);
+        })
+        .catch((error) => {
+          console.error("Error fetching product data:", error);
+        });
+    }
+    getAPI();
+  }, []);
+
   return (
     <div className="App">
-      <h1 className="text-sky-500 dark:text-sky-400 text-lg font-medium"> Haroldcyclopedia</h1>
-      <p className="text-3xl font-bold underline md:flex bg-red-100 rounded-xl p-8">here</p>
+      <h1 className="text-sky-500 dark:text-sky-400 text-lg font-medium">
+        {" "}
+        Haroldcyclopedia
+      </h1>
+      <p className="text-3xl font-bold underline md:flex bg-red-100 rounded-xl p-8">
+        here
+      </p>
       <button className="text-sky-500 dark:text-sky-400 text-lg">Submit</button>
       <Search />
     </div>
@@ -31,20 +38,3 @@ function App() {
 }
 
 export default App;
-
-
-/* 
-
-article_id - random
-
-article_name
-article_subject (react / html / css)
-article_type (pdf / youtube / book / kindle) */
-
-/* tailwind */
-
-/* header svg, main - together */
-
-/* Nav Bar - vandhana */
-
-/* Footer - Keeanu */
